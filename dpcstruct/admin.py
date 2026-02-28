499c44005f47769013e5bd65b5a78a76965dfe23
Emmanuel Nyandu Kagarabi <emmanuelnk@aims.ac.za>
2026-02-28 18:07:56 +0000

    Register DpcStructMcsProperty model in admin panel

--- dpcstruct/admin.py
+++ dpcstruct/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import DpcStructMcsProperty
 
-# Register your models here.
+
+@admin.register(DpcStructMcsProperty)
+class DpcStructMcsPropertyAdmin(admin.ModelAdmin):
+	list_display = (
+		'mc_id',
+		'mc_size',
+		'plddt',
+		'tmscore',
+		'pident',
+		'pfam_score',
+        'pfam_labels',
+	)
+	search_fields = ('mc_id', 'pfam_labels')
--- dpcstruct/urls.py
+++ dpcstruct/urls.py
@@ -3,6 +3,7 @@ from .views import DpcStructPropertyListView
 
 urlpatterns = [
     path('', DpcStructPropertyListView.as_view(), name='index'),
+    path('mcs/', DpcStructPropertyListView.as_view(), name='dpcstruct_mcs_list'),
     # Placeholder for detail view
     path('dpcstruct/<str:mc_id>/', DpcStructPropertyListView.as_view(), name='dpcstruct_detail'),
 ]
